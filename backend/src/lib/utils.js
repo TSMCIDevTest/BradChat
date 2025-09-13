750ee9ab4b72567f7f3340da79a2835bb8584d39
TSMCIDevTest <starcodebrad2023@outlook.com>
2025-09-13 15:49:06 -0700

    fix signup

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,10 +1,14 @@
 import jwt from 'jsonwebtoken';
 
 export const generateToken = (user,res) => {
+    const { JWT_SECRET } = process.env;
+    if(!JWT_SECRET) {
+        throw new Error("JWT_SECRET is not configured");
+    }
     const token = jwt.sign({ UserId: user._id}, process.env.JWT_SECRET, {
         expiresIn: "7d",
     })
-    res.cookie("token", token, {
+    res.cookie("jwt", token, {
         maxAge: 7*24*60*60*1000, // 7 days
         httpOnly: true,
         secure: process.env.NODE_ENV === "devlopment" ? false : true, // set to true in production
