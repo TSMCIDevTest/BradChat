e64808b1ae940e0cfb9af01028d10c84def0a6ac
TSMCIDevTest <starcodebrad2023@outlook.com>
2025-09-13 15:20:30 -0700

    signup controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,12 @@ import path from 'path';
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
+import { connectDB } from './lib/db.js';
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 const __dirname = path.resolve();
 
@@ -27,4 +29,5 @@ if (process.env.NODE_ENV === "production") {
 
 app.listen(PORT, () => {
   console.log("Server is running on port: " + PORT);
+  connectDB();
 });
