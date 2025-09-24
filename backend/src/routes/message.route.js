e9f377d34e28426af901279db06f3383331d01d2
TSMCIDevTest <starcodebrad2023@outlook.com>
2025-09-24 12:53:47 -0700

    Routes

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import { getAllContacts, getMessagesByUserId, sendMessage, getChatPartners } from '../controllers/message.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
-router.get('/send', (req, res) => {
-  // Handle sending a message
-  res.send('Send message endpoint');
-});
+router.get("/contacts", protectRoute, getAllContacts);
+router.get("/chats", protectRoute, getChatPartners);
+router.get("/:id", protectRoute, getMessagesByUserId);
+router.post('/send/:id', protectRoute, sendMessage);
 
 export default router;
